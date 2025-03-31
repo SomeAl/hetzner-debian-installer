6b40826f3d711c5411437e9e04732735a8cc8808
Your Name <you@example.com>
2025-03-31 20:16:36 +0300

    clean: add 2 step clean

--- clean.bash
+++ clean.bash
@@ -1,3 +1,13 @@
+#clear raid install step
 mdadm --stop /dev/md*
 wipefs -a /dev/nvme{0,1}n1
 mdadm --detail --scan >> /etc/mdadm/mdadm.conf
+
+#clear dabain install step
+umount /mnt/md0p1
+umount /mnt/md0p2
+umount /mnt/md0p3
+
+rm -rf /mnt/md0p1
+rm -rf /mnt/md0p2
+rm -rf /mnt/md0p3
