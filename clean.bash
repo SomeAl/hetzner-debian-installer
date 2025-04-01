065e4ed932cd4f5268c5be312272f7ed272c2e29
Your Name <you@example.com>
2025-04-01 19:34:19 +0300

    cleaner fix

--- clean.bash
+++ clean.bash
@@ -1,8 +1,3 @@
-#clear raid install step
-mdadm --stop /dev/md*
-wipefs -a /dev/nvme{0,1}n1
-mdadm --detail --scan >> /etc/mdadm/mdadm.conf
-
 #clear dabain install step
 umount /mnt/md0p1
 umount /mnt/md0p2
@@ -11,3 +6,9 @@ umount /mnt/md0p3
 rm -rf /mnt/md0p1
 rm -rf /mnt/md0p2
 rm -rf /mnt/md0p3
+
+#clear raid install step
+mdadm --stop /dev/md*
+wipefs -a /dev/nvme{0,1}n1
+mdadm --detail --scan >> /etc/mdadm/mdadm.conf
+
