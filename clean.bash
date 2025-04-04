315979821281d3d8dc66a44e39315809988f5fb7
Your Name <you@example.com>
2025-04-04 11:02:10 +0300

    clean: disable swap

--- clean.bash
+++ clean.bash
@@ -1,3 +1,6 @@
+#disable swap
+swapoff /mnt/md0p2
+
 #clear dabain install step
 umount /mnt/md0p1
 umount /mnt/md0p2
@@ -8,7 +11,7 @@ rm -rf /mnt/md0p2
 rm -rf /mnt/md0p3
 
 #clear raid install step
-mdadm --stop /dev/md*
+mdadm --stop /dev/md0*
 wipefs -a /dev/nvme{0,1}n1
 mdadm --detail --scan >> /etc/mdadm/mdadm.conf
 
